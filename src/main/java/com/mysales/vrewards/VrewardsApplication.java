ffcf3e5a1276ed0387ca4aa48411105aa84426a9
vassu18 <65691622+vassu18@users.noreply.github.com>
2023-02-16 11:11:15 -0700

    intialcommit

--- src/main/java/com/mysales/vrewards/VrewardsApplication.java
+++ src/main/java/com/mysales/vrewards/VrewardsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VrewardsApplication {
 
 	public static void main(String[] args) {
+	
 		SpringApplication.run(VrewardsApplication.class, args);
 	}
 
