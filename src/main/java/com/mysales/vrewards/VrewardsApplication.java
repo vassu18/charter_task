5b9bff123d51cfaf2a8012be6372ab9c5bd9c1ef
vassu18 <65691622+vassu18@users.noreply.github.com>
2023-02-16 11:36:50 -0700

    assignment for charter communications from Vasavi lingala
    assignment for charter communications from Vasavi lingala

---
+++ src/main/java/com/mysales/vrewards/VrewardsApplication.java
@@ -0,0 +1,15 @@
+package com.mysales.vrewards;
+
+// Author vasavi lingala, 2023.0216
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrewardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysales/vrewards/VrewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysales.vrewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
