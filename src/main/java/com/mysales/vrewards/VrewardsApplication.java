ca48dc025955a0d9a520cf0551d566d76e1493c6
vassu18 <65691622+vassu18@users.noreply.github.com>
2023-02-16 11:05:54 -0700

    Charter communications task

---
+++ src/main/java/com/mysales/vrewards/VrewardsApplication.java
@@ -0,0 +1,13 @@
+package com.mysales.vrewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VrewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VrewardsApplication.class, args);
+	}
+
+}
